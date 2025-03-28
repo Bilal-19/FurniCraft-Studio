a7e87b0a21d4d8e4793f0fba586008080870cd25
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-03-28 15:19:02 +0500

    Fixed footer.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,25 +4,33 @@ export default function Footer() {
     return (
         <>
             <div className="w-full bg-[#111827] pt-10 pb-10">
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
-                    <div className="m-5 md:m-10">
+                <div className="flex flex-col md:flex-row justify-between items-start">
+                    <div className="w-80 md:w-2/5 m-2 md:m-5">
                         <img src="/footer_logo.png" alt="logo" />
                         <p className="text-[#9CA3AF]">
-                            Transforming spaces with exceptional
-                            design and quality furniture since 2010.
+                            Transform your space into something
+                            extraordinary with our premium interior
+                            design services.
                         </p>
                     </div>
 
-                    <div className="text-white space-y-1 m-5 md:m-10">
-                        <h5 className="font-medium text-lg">Company</h5>
-                        <p>About Us</p>
-                        <p>Services</p>
-                        <p>Products</p>
-                        <p>Testimonials</p>
-                        <p>Contact Us</p>
+                    <div className="w-80 md:w-1/5 text-white space-y-1 m-2 md:m-5">
+                        <h5 className="font-medium text-lg">Contact</h5>
+                        <p className="text-[#9CA3AF]">923427634247</p>
+                        <p className="text-[#9CA3AF]">info@furnicraftstudio.com</p>
+                        <p className="text-[#9CA3AF]">Mon-Fri, 9:00 AM - 6:00 PM</p>
                     </div>
 
-                    <div className="text-white space-y-1 m-5 md:m-10">
+                    <div className="w-80 md:w-1/5 text-white space-y-1 m-2 md:m-5">
+                        <h5 className="font-medium text-lg">Company</h5>
+                        <p className="text-[#9CA3AF]">About Us</p>
+                        <p className="text-[#9CA3AF]">Services</p>
+                        <p className="text-[#9CA3AF]">Products</p>
+                        <p className="text-[#9CA3AF]">Testimonials</p>
+                        <p className="text-[#9CA3AF]">Contact Us</p>
+                    </div>
+
+                    <div className="w-80 md:w-1/5 text-white space-y-1 m-2 md:m-5">
                         <h5 className="font-medium text-lg">Connect</h5>
                         <div className="space-x-5">
                             <i className="fa-brands fa-instagram"></i>
