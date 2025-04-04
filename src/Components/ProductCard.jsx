28dca59ccba91a0fac2d8720079b97413049bf3c
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-04-04 15:33:50 +0500

    Added animation to Products page.

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import {motion} from "framer-motion"
 
 export default function ProductCard(props) {
     return (
         <>
-            <div className="w-80 md:w-100 ff-roboto mb-12 mx-auto md:m-5">
+            <motion.div initial={{ opacity: 0, y: 50 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.5 }}
+                    viewport={{ once: true }} className="w-80 md:w-100 ff-roboto mb-12 mx-auto md:m-5">
                 <img src={props.image_path} alt={props.product_name} className="w-100 h-60 object-cover rounded-md hover:scale-102 duration-200" />
                 <h5 className="font-medium text-lg md:mb-2 mt-3">{props.product_name}</h5>
                 <p className="text-[#4B5563] md:mb-2">{props.product_description}</p>
                 <p className="text-xl font-medium">${props.product_price}</p>
-            </div>
+            </motion.div>
         </>
     )
 }
