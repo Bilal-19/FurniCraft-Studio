070995527210e05d4dc6a4c6740660fd336d155e
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-04-05 10:39:09 +0500

    Fixed footer logo.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,8 +6,8 @@ export default function Footer() {
         <>
             <div className="w-full bg-[#111827] pt-10 pb-10 ff-roboto">
                 <div className="flex flex-col md:flex-row justify-between items-start">
-                    <div className="w-80 md:w-2/5 m-2 md:m-5">
-                        <img src="/footer_logo.png" alt="logo" />
+                    <div className="w-80 md:w-2/5 m-3 md:m-5">
+                        <img src="/footer_logo.png" alt="logo" className="h-16 object-contain" />
                         <p className="text-[#9CA3AF]">
                             Transform your space into something
                             extraordinary with our premium interior
@@ -15,14 +15,14 @@ export default function Footer() {
                         </p>
                     </div>
 
-                    <div className="w-80 md:w-1/5 text-white space-y-1 m-2 md:m-5">
+                    <div className="w-80 md:w-1/5 text-white space-y-1 m-3 md:m-5">
                         <h5 className="font-medium text-lg">Contact</h5>
                         <p className="text-[#9CA3AF]">+92 3082491543</p>
                         <p className="text-[#9CA3AF]">contact@furnicraft.com</p>
                         <p className="text-[#9CA3AF]">Mon-Fri, 9:00 AM - 6:00 PM</p>
                     </div>
 
-                    <div className="w-80 md:w-1/5 text-white m-2 md:m-5 flex flex-col">
+                    <div className="w-80 md:w-1/5 text-white m-3 md:m-5 flex flex-col">
                         <h5 className="font-medium text-lg">Company</h5>
                         <Link to="/about" className="text-[#9CA3AF]">About</Link>
                         <Link to="/services" className="text-[#9CA3AF]">Services</Link>
@@ -31,7 +31,7 @@ export default function Footer() {
                         <Link to="/contact" className="text-[#9CA3AF]">Contact Us</Link>
                     </div>
 
-                    <div className="w-80 md:w-1/5 text-white space-y-1 m-2 md:m-5">
+                    <div className="w-80 md:w-1/5 text-white space-y-1 m-3 md:m-5">
                         <h5 className="font-medium text-lg">Connect</h5>
                         <div className="space-x-5">
                             <i className="text-[#9CA3AF] fa-brands fa-instagram"></i>
